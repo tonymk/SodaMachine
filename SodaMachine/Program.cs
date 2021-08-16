6d1c4f59e69fad1c70d1a4efb886a2b382b145f3
Tony Krøger <tony.kroger@gmail.com>
2021-08-16 16:12:23 +0200

    init, upgraded to .net core

---
+++ SodaMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SodaMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
